6bc688206d6d059fbf52f85283c93b7f952be61a
Andre <andreherrera97@hotmail.com>
2020-02-11 23:14:06 -0500

    Adding CORS support

--- kbsbot/nlpengine/app.py
+++ kbsbot/nlpengine/app.py
@@ -1,7 +1,7 @@
 import os
 from flakon import create_app as _create_app
 from .views import blueprints
-# from flask_cors import CORS
+from flask_cors import CORS
 
 _HERE = os.path.dirname(__file__)
 _SETTINGS = os.path.join(_HERE, 'settings.ini')
@@ -12,6 +12,5 @@ def create_app(settings=None):
         settings = _SETTINGS
 
     app = _create_app(blueprints=blueprints, settings=settings)
-    # CORS(app)
+    CORS(app)
     return app
-
